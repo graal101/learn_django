962cac5fd7ac82e6101b856d698442ee8c6af3b6
Andrew Andrey <graal101@mail.ru>
2025-07-20 13:49:42 +0700

    deleting

--- women/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def index(request):  # HttpRequest Содержит информацию о запросе 
-    return HttpResponse("Страница приложения women")
-    
-def categories(request):
-    return HttpResponse("<h1>Статьи по категориям</h1>")
